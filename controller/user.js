a4aa1e94a340b6e2acfdffe2e427913a89d11018
jwchan1996 <741755613@qq.com>
2019-05-21 19:58:27 +0800

    【Add】控制器模块

---
+++ controller/user.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: jwchan1996
+ * @Date: 2019-05-21 19:56:04
+ * @LastEditors: jwchan1996
+ * @LastEditTime: 2019-05-21 19:57:54
+ */
+const userCode = require('../code/user')
+
+module.exports = {
+  
+}
