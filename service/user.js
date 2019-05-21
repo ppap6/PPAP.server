52379ad88944a4d6dcb1419f6c17a3b43d890de7
jwchan1996 <741755613@qq.com>
2019-05-21 20:01:11 +0800

    【Add】业务模块

---
+++ service/user.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: jwchan1996
+ * @Date: 2019-05-21 20:00:06
+ * @LastEditors: jwchan1996
+ * @LastEditTime: 2019-05-21 20:00:57
+ */
+const userModel = require('../model/user')
+const userCode = require('../code/user')
+
+const user ={
+
+}
+
+module.exports = user
