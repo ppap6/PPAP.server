43ef0ce27d64cf666d190a86543f3a6b444475da
jwchan1996 <741755613@qq.com>
2019-06-01 12:28:47 +0800

    【Feat】配置路由处理

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const Koa = require('koa')
+const path = require('path')
+const KoaRouter = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 const app = new Koa()
+const router = new KoaRouter()
 
-app.use( async(ctx) => {
-    ctx.body = "hello world"
+app.use(bodyParser())
+
+router.get('/', async(ctx) => {
+    ctx.body = `我是首页面,我的文件路径是在${path.join(__dirname)}下的app.js`
 })
-app.listen(2333)
-console.log("koa demo is run on localhost:2333")
+
+// 配置路由模块
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(2333, () => console.log("koa demo is run on localhost:2333"))
