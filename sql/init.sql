34cdd339ac6a567ac9ca08046b3a25907c55e071
jwchan1996 <741755613@qq.com>
2019-06-04 23:57:28 +0800

    【Docs】增加sql脚本

---
+++ sql/init.sql
@@ -0,0 +1,12 @@
+/*
+ * @Author: jwchan1996
+ * @Date: 2019-06-04 23:49:30
+ * @LastEditors: jwchan1996
+ * @LastEditTime: 2019-06-04 23:49:30
+ */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+    
+)
