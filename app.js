cf819ca5385e193dbba8a02d80b8ff6f826aa9b2
jwchan1996 <741755613@qq.com>
2019-06-06 17:56:48 +0800

    style : 增加注释

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = new Koa()
 
 const router = require('./router')
 
+//配置ctx.body解析中间件
 app.use(bodyParser())
 
 //初始化路由中间件
 app.use(router.routes()).use(router.allowedMethods())
 
+//监听启动窗口
 app.listen(2333, () => console.log("koa demo is run on localhost:2333"))
