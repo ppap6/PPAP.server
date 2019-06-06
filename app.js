1b0aae32c98a047e23d07c170ef895e3cac32a32
jwchan1996 <741755613@qq.com>
2019-06-06 14:28:08 +0800

    【Refactor】抽离及拆分路由

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const Koa = require('koa')
 const path = require('path')
-const KoaRouter = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const app = new Koa()
-const router = new KoaRouter()
+
+const router = require('./router')
 
 app.use(bodyParser())
 
-router.get('/', async(ctx) => {
-    ctx.body = `我是首页面,我的文件路径是在${path.join(__dirname)}下的app.js`
-})
-
-// 配置路由模块
+//初始化路由中间件
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(2333, () => console.log("koa demo is run on localhost:2333"))
--- router/index.js
+++ router/index.js
@@ -2,5 +2,25 @@
  * @Author: jwchan1996
  * @Date: 2019-05-21 20:02:00
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-05-21 20:02:16
+ * @LastEditTime: 2019-06-06 14:24:16
  */
+
+/**
+ * 整合所有子路由
+ */
+
+const router = require('koa-router')()
+
+// const user = require('./user')
+// const post = require('./post')
+// const topic = require('./topic')
+
+router.get('/', async(ctx) => {
+  ctx.body = '403 forbidden'
+})
+
+// router.use('/user', user.routes(), user.allowedMethods())
+// router.use('/post', post.routes(), post.allowedMethods())
+// router.use('/topic', topic.routes(), topic.allowedMethods())
+
+module.exports = router
