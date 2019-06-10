548968e84c6830dca339a63cfa092f03369aa392
jwchan1996 <741755613@qq.com>
2019-06-10 16:45:32 +0800

    【Feat】完善Mongo操作函数

--- model/post.js
+++ model/post.js
@@ -2,7 +2,7 @@
  * @Author: jwchan1996
  * @Date: 2019-05-21 19:58:41
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-06-10 15:23:29
+ * @LastEditTime: 2019-06-10 16:20:07
  */
 
 const db = require('../util/db_mongo')
@@ -10,7 +10,7 @@ const db = require('../util/db_mongo')
 const post = {
   //查找所有评论
   async getPostComment(){
-    let result = await db.select('comment')
+    let result = await db.find('comment')
     if(Array.isArray(result) && result.length > 0){
       return result
     }
--- util/db_mongo.js
+++ util/db_mongo.js
@@ -2,14 +2,19 @@
  * @Author: jwchan1996
  * @Date: 2019-06-10 11:09:36
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-06-10 15:33:12
+ * @LastEditTime: 2019-06-10 16:44:40
  */
 
 const MongoClient = require('mongodb').MongoClient
 
 const url = 'mongodb://localhost:27017/'
 
-//连接数据库并执行命令
+
+/**
+ * @description: 连接数据库并执行命令
+ * @param {Function} command 命令函数
+ * @return {Object} 返回
+ */
 const query = command => {
 
   return new Promise(( resolve, reject ) => {
@@ -26,10 +31,138 @@ const query = command => {
 
 }
 
-//查询函数
-const select = ( table, val={} ) => {
+
+/**
+ * @description: 查询通用函数
+ * @param {String} table 数据表
+ * @param {Object} condition 查询条件
+ * @return {Array} 返回
+ */
+const find = ( table, condition={} ) => {
   let command = (resolve, reject, client) => {
-    client.collection(table).find(val).toArray((err, result) => {
+    client.collection(table).find(condition).toArray((err, result) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    })
+  }
+  return query(command)
+}
+
+
+/**
+ * @description: 插入一条数据
+ * @param {String} table 数据表
+ * @param {Object} value 数据对象
+ * @return {boolean} 返回
+ */
+const insertOne = (table, value) => {
+  let command = (resolve, reject, client) => {
+    client.collection(table).insertOne(value, (err, result) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    })
+  }
+  return query(command)
+}
+
+
+/**
+ * @description: 插入多条数据
+ * @param {String} table 数据表
+ * @param {Array} value 数据对象数组
+ * @return {boolean} 返回
+ */
+const insertMany = (table, value) => {
+  let command = (resolve, reject, client) => {
+    client.collection(table).insertMany(value, (err, result) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    })
+  }
+  return query(command)
+}
+
+
+/**
+ * @description: 更新一条数据
+ * @param {String} table 数据表
+ * @param {Object} condition 查询条件
+ * @param {Object} value 数据对象，格式 {$set: {"type": "en"}}
+ * @return {boolean} 返回
+ */
+const updateOne = (table, condition, value) => {
+  let command = (resolve, reject, client) => {
+    client.collection(table).updateOne(condition, value, (err, result) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    })
+  }
+  return query(command)
+}
+
+
+/**
+ * @description: 更新多条数据
+ * @param {String} table 数据表
+ * @param {Object} condition 查询条件
+ * @param {Object} value 数据对象，格式 {$set: {"type": "en"}}
+ * @return {boolean} 返回
+ */
+const updateMany = (table, condition, value) => {
+  let command = (resolve, reject, client) => {
+    client.collection(table).updateMany(condition, value, (err, result) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    })
+  }
+  return query(command)
+}
+
+
+/**
+ * @description: 删除一条数据
+ * @param {String} table 数据表
+ * @param {Object} condition 查询条件
+ * @return {boolean} 返回
+ */
+const deleteOne = (table, condition) => {
+  let command = (resolve, reject, client) => {
+    client.collection(table).deleteOne(condition, (err, result) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    })
+  }
+  return query(command)
+}
+
+
+/**
+ * @description: 删除多条数据
+ * @param {String} table 数据表
+ * @param {Object} condition 查询条件
+ * @return {boolean} 返回
+ */
+const deleteMany = (table, condition) => {
+  let command = (resolve, reject, client) => {
+    client.collection(table).deleteMany(condition, (err, result) => {
       if(err){
         reject(err)
       }else{
@@ -42,5 +175,11 @@ const select = ( table, val={} ) => {
 
 module.exports = {
   query,
-  select
+  find,
+  insertOne,
+  insertMany,
+  updateOne,
+  updateMany,
+  deleteOne,
+  deleteMany
 }
