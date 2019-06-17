5e430b90f970d4242a9edd658d8bcd82379c6806
jwchan1996 <741755613@qq.com>
2019-06-17 19:59:02 +0800

    【Feat】完善user接口

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@
  * @Author: jwchan1996
  * @Date: 2019-05-21 19:58:41
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-06-16 23:31:54
+ * @LastEditTime: 2019-06-17 19:50:21
  */
 
 const db = require('../util/db')
@@ -41,6 +41,25 @@ const user = {
   async deleteUser(id){
     let sql = 'DELETE FROM user WHERE id=??'
     let result = await db.query(sql, [id])
+    if(result){
+      return true
+    }
+    return false
+  },
+
+  //修改用户信息
+  async updateUser(id, data){
+    let sql = 'UPDATE user SET name=??,account=??,password=??,sex=??,email=??,create_time=??,role_id=?? WHERE id=??'
+    let values = [
+      data.name,
+      data.account,
+      data.password,
+      data.sex,
+      data.email,
+      data.create_time,
+      data.role_id
+    ]
+    let result = await db.query(sql, [...values,id])
     if(result){
       return true
     }
