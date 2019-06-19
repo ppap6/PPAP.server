1fb37dd53189951460d69e2920cf70265801cb41
jwchan1996 <741755613@qq.com>
2019-06-20 01:00:11 +0800

    【Feat】post获取帖子数据（页码，数目，话题id）

--- model/post.js
+++ model/post.js
@@ -2,16 +2,29 @@
  * @Author: jwchan1996
  * @Date: 2019-05-21 19:58:41
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-06-19 23:27:44
+ * @LastEditTime: 2019-06-20 00:58:01
  */
 
 const db = require('../util/db')
 const db_mongo = require('../util/db_mongo')
 
 const post = {
-  //获取帖子
-  async getPostList(){
-    let sql = 'SELECT * FROM post'
+
+  //获取帖子数据（页数，数目，话题id）
+  async getPostList(pageNum=1,pageSize=20,topicId=0){
+    let start = (pageNum-1)*pageSize
+    let sql
+    if(topicId === 0){
+      sql = `SELECT p.id,p.uid,u.name AS uname,p.title,p.content,p.create_time,p.update_time,p.reads,p.likes,p.collects,p.topic_id,t.name AS topic_name 
+        FROM post AS p,user AS u,topic AS t 
+        WHERE p.topic_id=t.id AND p.uid=u.id
+        LIMIT ${start},${pageSize}`
+    }else{
+      sql = `SELECT p.id,p.uid,u.name AS uname,p.title,p.content,p.create_time,p.update_time,p.reads,p.likes,p.collects,p.topic_id,t.name AS topic_name 
+        FROM post AS p,user AS u,topic AS t 
+        WHERE p.topic_id=t.id AND p.uid=u.id AND t.id=${topicId}
+        LIMIT ${start},${pageSize}`
+    }
     let result = await db.query(sql)
     if(Array.isArray(result) && result.length > 0){
       return result
