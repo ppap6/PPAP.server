6e64c745a261baebd21598d0de2f188405a28c8b
jwchan1996 <741755613@qq.com>
2019-07-11 23:27:22 +0800

    【Fix】comment获取评论信息

--- router/comment.js
+++ router/comment.js
@@ -2,7 +2,7 @@
  * @Author: jwchan1996
  * @Date: 2019-07-07 23:24:57
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-07-11 00:37:47
+ * @LastEditTime: 2019-07-11 23:25:35
  */
 
 const router = require('koa-router')()
@@ -11,7 +11,7 @@ const comment = require('../controller/comment')
 router
   .get('/:id', comment.getCommentList)    //获取帖子评论列表
   .post('/', comment.addComment)     //添加帖子评论
-  .get('/:id', comment.getComment)    //获取帖子评论信息
+  .get('/detail/:id', comment.getComment)    //获取帖子评论信息
   .put('/:id', comment.updateComment)     //修改帖子评论信息
 
 module.exports = router
