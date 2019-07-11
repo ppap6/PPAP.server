dde49a4c0396dd1d8cf4dca963a37f341ca4b0af
jwchan1996 <741755613@qq.com>
2019-07-11 23:45:42 +0800

    【Refactor】comment调整路由

--- router/comment.js
+++ router/comment.js
@@ -2,16 +2,16 @@
  * @Author: jwchan1996
  * @Date: 2019-07-07 23:24:57
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-07-11 23:25:35
+ * @LastEditTime: 2019-07-11 23:40:40
  */
 
 const router = require('koa-router')()
 const comment = require('../controller/comment')
 
 router
-  .get('/:id', comment.getCommentList)    //获取帖子评论列表
+  .get('/post/:id', comment.getCommentList)    //获取帖子评论列表
   .post('/', comment.addComment)     //添加帖子评论
-  .get('/detail/:id', comment.getComment)    //获取帖子评论信息
+  .get('/:id', comment.getComment)    //获取帖子评论信息
   .put('/:id', comment.updateComment)     //修改帖子评论信息
 
 module.exports = router
