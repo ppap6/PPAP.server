5a65f04bfc97e1e953da677740df7b4f67b89b1c
jwchan1996 <741755613@qq.com>
2019-10-07 21:45:06 +0800

    【Fix】优化时间字符串转化函数

--- util/index.js
+++ util/index.js
@@ -2,17 +2,17 @@
  * @Author: jwchan1996
  * @Date: 2019-05-21 20:03:18
  * @LastEditors: jwchan1996
- * @LastEditTime: 2019-06-21 17:43:32
+ * @LastEditTime: 2019-10-07 21:38:36
  */
 
 //修改标准时间为日期时间字符串
 const changeTimeToStr = (time) => {
   let year = time.getFullYear()  
-  let month = time.getMonth()+1     
-  let day = time.getDate()      
-  let hour = time.getHours()      
-  let minute = time.getMinutes()     
-  let second = time.getSeconds() 
+  let month = time.getMonth()+1 < 10 ? "0"+(time.getMonth()+1) : time.getMonth()+1    
+  let day = time.getDate() < 10 ? "0"+time.getDate() : time.getDate()    
+  let hour = time.getHours() < 10 ? "0"+time.getHours() : time.getHours()       
+  let minute = time.getMinutes() < 10 ? "0"+time.getMinutes() : time.getMinutes()     
+  let second = time.getSeconds() < 10 ? "0"+time.getSeconds() : time.getSeconds()
   let newDate = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second     
   return newDate
 }
