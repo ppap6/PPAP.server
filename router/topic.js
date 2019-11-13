e3b2d4b7a691502170969a396749b9584ce17556
jwchan1996 <741755613@qq.com>
2019-11-13 11:24:22 +0800

    【Fix】更改添加话题路由

--- router/topic.js
+++ router/topic.js
@@ -11,7 +11,7 @@ const topic = require('../controller/topic')
 router
     .get('/', topic.getTopicList)   //获取话题列表
     .get('/admin', topic.getTopicListForAdmin)   //管理运营获取话题列表
-    .post('/', topic.addTopic)   //添加话题
+    .post('/add', topic.addTopic)   //添加话题
     .get('/:id', topic.getTopic)   //获取话题信息
     .del('/:id', topic.deleteTopic)   //删除话题
     .put('/:id', topic.updateTopic)    //修改话题信息
