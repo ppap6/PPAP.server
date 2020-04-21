938db2c6d3ad53295d2f674eeafe9b0f62ee5c5b
jwchan1996 <741755613@qq.com>
2020-04-21 19:32:11 +0800

    【Fix】邮件配置

---
+++ config/email.js
@@ -0,0 +1,18 @@
+//邮箱相关
+const Email = {
+  host: 'smtp.qq.com',
+  user: 'ppaplive@qq.com',
+  pass: 'xxxx',
+  code(){
+    return Math.random().toString(16).slice(2, 6).toUpperCase()
+  },
+  expire() {
+    return new Date().getTime() + 60 * 1000
+  },
+  // smtp_service: '163',
+  stmp_service: 'qq',
+  smtp_port: 465,
+  smtp_secure_connection: true
+}
+
+module.exports = Email
